77ed9d68ad372ed5b23f3fbf3ef61e241d9ed02b
Maria Eduarda <133064900+MariaEduardaFB@users.noreply.github.com>
2023-12-23 19:14:11 -0300

    fix: trying to fix the javascript

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const inputBox = document.getElementById("input-box")
 const listContainer = document.getElementById("list-container")
+
 function addTask() {
     if(inputBox.value ===""){
         alert("You must write something!")
@@ -22,3 +23,11 @@ listContainer.addEventListener("click", function(e){
         e.target.parentElement.remove()
     }
 }, false)
+
+
+
+
+
+
+
+
