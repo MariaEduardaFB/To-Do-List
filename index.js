7dfa6ff074833408cbc2b841f167416854099b4a
Maria Eduarda <133064900+MariaEduardaFB@users.noreply.github.com>
2023-12-22 22:38:35 -0300

    feat: trying to fix a bug

--- index.js
+++ index.js
@@ -16,6 +16,9 @@ function addTask() {
 
 listContainer.addEventListener("click", function(e){
     if(e.target.tagName === "LI"){
-        e.target.classList
+        e.target.classList.toggle("checked");
     }
-})
+    else if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove()
+    }
+}, false)
