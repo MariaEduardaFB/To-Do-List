b04dc903519d13e174eb1b20b15b2961e000f089
Maria Eduarda <133064900+MariaEduardaFB@users.noreply.github.com>
2023-12-28 00:03:10 -0300

    feat: adding the remove button

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -13,12 +13,19 @@ add.addEventListener("click", () => {
         const colorButton = document.createElement("button");
         colorButton.innerHTML = "";
         colorButton.classList.add("color-button");
+        colorButton.addEventListener("click", () => toggleColor(newElement));
         newElement.appendChild(colorButton);
 
         const taskText = document.createElement("span");
         taskText.innerHTML = inputBox.value;
         newElement.appendChild(taskText);
 
+        // Adiciona o botão de remoção
+        const removeButton = document.createElement("button");
+        removeButton.innerHTML = "✖";
+        removeButton.classList.add("remove-tasks");
+        removeButton.addEventListener("click", () => removeTask(newElement));
+        newElement.appendChild(removeButton);
 
         list.appendChild(newElement);
         tasks.push({
@@ -33,6 +40,25 @@ const updateLocalStorage = () => {
     localStorage.setItem("tasks", JSON.stringify(tasks));
 }
 
+const toggleColor = (taskElement) => {
+    const colorButton = taskElement.querySelector(".color-button");
+    colorButton.classList.toggle("clicked");
+    updateLocalStorage();
+}
+
+const removeTask = (taskElement) => {
+    const taskText = taskElement.querySelector("span").innerHTML;
+    
+    // Remove a tarefa do array tasks
+    tasks = tasks.filter(task => task.text !== taskText);
+    
+    // Remove a tarefa do localStorage
+    updateLocalStorage();
+    
+    // Remove a tarefa do DOM
+    taskElement.remove();
+}
+
 const localStorageTasks = localStorage.getItem('tasks');
 if(localStorageTasks){
     tasks = JSON.parse(localStorageTasks);
@@ -43,11 +69,19 @@ if(localStorageTasks){
         const colorButton = document.createElement("button");
         colorButton.innerHTML = "";
         colorButton.classList.add("color-button");
+        colorButton.addEventListener("click", () => toggleColor(newElement));
         newElement.appendChild(colorButton);
 
         const taskText = document.createElement("span");
         taskText.innerHTML = task.text;
         newElement.appendChild(taskText);
+
+        // Adiciona o botão de remoção
+        const removeButton = document.createElement("button");
+        removeButton.innerHTML = "✖";
+        removeButton.classList.add("remove-tasks");
+        removeButton.addEventListener("click", () => removeTask(newElement));
+        newElement.appendChild(removeButton);
 
         // Adiciona a classe "clicked" se a tarefa estiver colorizada
         if (task.colorized) {
@@ -57,10 +91,3 @@ if(localStorageTasks){
         list.appendChild(newElement);
     });
 }
-
-// Adiciona o event listener para o botão "Colorize" dentro do loop forEach
-document.querySelectorAll(".color-button").forEach((button) => {
-    button.addEventListener("click", () => {
-        button.classList.toggle("clicked");
-    });
-});
