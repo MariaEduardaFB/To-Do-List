fc3f0d7339ade855ce67e8a1de580edba1aeee6b
Maria Eduarda <133064900+MariaEduardaFB@users.noreply.github.com>
2023-12-27 23:33:34 -0300

    feat: adding the checked button

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,37 +1,66 @@
-const list = document.getElementById("list-container") //pega elemento pelo id e atribui a variável list
-const add = document.getElementById("add-button") //pega elemento pelo id e atribui a variável add
-const inputBox = document.getElementById("input-box") //pega o campo de entrada de texto
-let tasks = [] //inicia um array vazio pra armazenar as tasks
+const list = document.getElementById("list-container");
+const add = document.getElementById("add-button");
+const inputBox = document.getElementById("input-box");
+let tasks = [];
 
 add.addEventListener("click", () => { 
     if (inputBox.value === "") {
         alert("You must write something!");
     } else {
-    const newElement = document.createElement("li") //cria novo elemento em memória
-    newElement.innerHTML = inputBox.value//altera o valor do elemento html criado para algo visível
-    list.appendChild(newElement)//atribui o novo elemento como filho do elemento list
+        const newElement = document.createElement("li");
 
-    tasks.push(inputBox.value)// adiciona o valor das tasks no array 
-    updateLocalStorage()
+        // Adiciona o botão "Colorize" antes do texto da tarefa
+        const colorButton = document.createElement("button");
+        colorButton.innerHTML = "";
+        colorButton.classList.add("color-button");
+        newElement.appendChild(colorButton);
+
+        const taskText = document.createElement("span");
+        taskText.innerHTML = inputBox.value;
+        newElement.appendChild(taskText);
+
+
+        list.appendChild(newElement);
+        tasks.push({
+            text: inputBox.value,
+            colorized: false
+        });
+        updateLocalStorage();
     }
-})
+});
 
 const updateLocalStorage = () => {
-    localStorage.setItem("tasks", JSON.stringify(tasks))
+    localStorage.setItem("tasks", JSON.stringify(tasks));
 }
 
-const localStorageTasks = localStorage.getItem('tasks')
+const localStorageTasks = localStorage.getItem('tasks');
 if(localStorageTasks){
-    tasks = JSON.parse(localStorageTasks)
+    tasks = JSON.parse(localStorageTasks);
     tasks.forEach((task) => {
-        const newElement = document.createElement("li") //cria novo elemento em memória
-        newElement.innerHTML = task//altera o valor do elemento html criado para algo visível
-        list.appendChild(newElement)
-    })
+        const newElement = document.createElement("li");
+
+        // Adiciona o botão "Colorize" antes do texto da tarefa
+        const colorButton = document.createElement("button");
+        colorButton.innerHTML = "";
+        colorButton.classList.add("color-button");
+        newElement.appendChild(colorButton);
+
+        const taskText = document.createElement("span");
+        taskText.innerHTML = task.text;
+        newElement.appendChild(taskText);
+
+        // Adiciona a classe "clicked" se a tarefa estiver colorizada
+        if (task.colorized) {
+            colorButton.classList.add("clicked");
+        }
+
+        list.appendChild(newElement);
+    });
 }
 
-
-
-
-
- 
+// Adiciona o event listener para o botão "Colorize" dentro do loop forEach
+document.querySelectorAll(".color-button").forEach((button) => {
+    button.addEventListener("click", () => {
+        button.classList.toggle("clicked");
+    });
+});
